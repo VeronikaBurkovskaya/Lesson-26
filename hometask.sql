bece16035c4aa3bb1d5302b3a2deaa06339dfe8b
VeronikaBurkovskaya <37294035+VeronikaBurkovskaya@users.noreply.github.com>
2022-11-30 20:12:39 +0200

    Add sql file

---
+++ hometask.sql
@@ -0,0 +1,9 @@
+insert into lesson_23.user (id, username,email,password)
+values (11, "serrgey", "sergeyr@gmail.com", "Passrword123");
+
+insert into lesson_23.cars (id, car_name,car_cost,user_id)
+values (3, "Audi", "55555", 1);
+
+insert into lesson_23.cars (id, car_name,car_cost,user_id,expense)
+values (6, "TT", "54555", 1, "43");
+
